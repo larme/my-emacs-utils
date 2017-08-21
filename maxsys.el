8bd266db28ed00a45b86b0aaf28aa396bf548110
Zhao Shenyang <dev@zsy.im>
2017-08-22 02:20:00 +0800

    use lexical binding by default

--- maxsys.el
+++ maxsys.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 ;;; define parameters of a max/msp system and export them to json files
 
 (require 'json)
--- zsy.el
+++ zsy.el
@@ -1,3 +1,4 @@
+;;; -*- lexical-binding: t -*-
 ;;; my common library
 
 (require 'seq)
